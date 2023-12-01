8b6e566114973e91fbfb671f2cdd29630db1f707
Brandon Hollenbeck <webrinseor@gmail.com>
2023-12-01 00:03:48 -0800

    button component

--- app/(breeders)/@modal/(.)puppies/[id]/page.tsx
+++ app/(breeders)/@modal/(.)puppies/[id]/page.tsx
@@ -2,7 +2,7 @@
 import { Modal } from "@/components/modal";
 import { ScrollArea } from "@radix-ui/react-scroll-area";
 import { AspectRatio } from "@radix-ui/themes";
-import { Button } from "@/components/button";
+import { ButtonLink } from "@/components/button";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -21,23 +21,34 @@ export default function PuppiesModal(){
                                     fill
                                     />
                             </AspectRatio>
-                            <div className="pt-4 text-center">
+                            <div className="pt-4 text-center space-y-2">
                                 <div>
                                     <p>Call today for availability</p>
+                                    <ButtonLink href="tel:555-555-5555" label='(555) 555-5555' className="m-auto" />
                                 </div>
-                                <Button href="tel:555-555-5555" label='(555) 555-5555' className="m-auto" />
-
-                                <Button label='(555) 555-5555' className="m-auto" />
+                                <div>
+                                    <p>- or -</p>
+                                </div>
+                                <ButtonLink href="/contact" label='Send Message' className="m-auto" />
                             </div>
                         </div>
                         <div className="w-2/3">
                             <h2 className="text-3xl">Cloud</h2>
-                            <div className="pt-4">
-                                Availability: <strong>Available</strong><br />
-                                Breed: <strong>Samoyed</strong><br />
-                                Sex: <strong>Female</strong><br />
-                                Date of Birth: <strong>Aug 26, 2023</strong><br />
-                                Price: <strong>$2,000</strong> 
+                            <div className="space-y-4">
+                                <div>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ut tincidunt libero, id dignissim tellus. 
+                                    Suspendisse consequat mollis ligula, at maximus orci volutpat et. Nulla id est metus. Donec eget fringilla lacus. 
+                                    Sed eget feugiat nisi. Mauris venenatis magna nunc, vel sagittis ligula aliquet non. Nullam interdum commodo 
+                                    suscipit. Fusce interdum risus et maximus lacinia. Vivamus vitae risus libero. In sit amet dui purus. Nulla sed 
+                                    commodo metus, quis rutrum nibh.
+                                </div>
+                                <div>
+                                    Availability: <strong>Available</strong><br />
+                                    Breed: <strong>Samoyed</strong><br />
+                                    Sex: <strong>Female</strong><br />
+                                    Date of Birth: <strong>Aug 26, 2023</strong><br />
+                                    Price: <strong>$2,000</strong> 
+                                </div>
                             </div>
                         </div>
                     </div>
--- components/button/Button.tsx
+++ components/button/Button.tsx
@@ -6,6 +6,32 @@ import Link from 'next/link';
 
 type ButtonSizes  = Pick<AMPThemeSizes,  'xs' | 'sm' | 'base' | 'lg' | 'xl'>;
 
+const IconLeft = ({
+    iconLeft,
+    iconColor
+}:{
+    iconLeft?:keyof AMPThemeIconProps;
+    iconColor?:keyof AMPThemeColors;
+}) => {
+    return (typeof iconLeft !== 'undefined') ? <span className="pointer-events-none flex items-center w-3 h-3"><Icon className={classNames(
+        Styles['btn-icon'],
+        Styles.icon
+    )} icon={iconLeft} color={iconColor} /></span>:<></>
+};
+
+const IconRight = ({
+    iconRight,
+    iconColor
+}:{
+    iconRight?:keyof AMPThemeIconProps;
+    iconColor?:keyof AMPThemeColors;
+}) => {
+    return (typeof iconRight !== 'undefined') ? <span className="pointer-events-none flex items-center w-3 h-3"><Icon className={classNames(
+        Styles['btn-icon'],
+        Styles.icon
+    )} icon={iconRight} color={iconColor} /></span>:<></>
+};
+
 export const Button = ({
     id,
     label,
@@ -40,36 +66,71 @@ export const Button = ({
     const btnColor = `btn-${color}`; 
     const btnSize = `btn-${size}`;
     const wFull = typeof full !== 'undefined' && 'w-full';
-    const ButtonType = (href) ? Link:'button';
 
-    const IconRight = () => {
-        return (typeof iconRight !== 'undefined') ? <span className="pointer-events-none flex items-center w-3 h-3"><Icon className={classNames(
-            Styles['btn-icon'],
-            Styles.icon
-        )} icon={iconRight} color={iconColor} /></span>:<></>
-    };
+    return ( 
+        <button 
+            id={id}
+            className={classNames(
+                wFull,
+                Styles.btn,
+                Styles[btnColor],
+                Styles[btnSize],
+                className,
+                onClick,
+                disabled,
+                type,
+            )}
+        >
+            <IconLeft iconLeft={iconLeft} iconColor={iconColor} />{label && <span>{label}</span>}<IconRight iconRight={iconRight} iconColor={iconColor} />
+        </button>
+    )
+}
 
-    const IconLeft = () => {
-        return (typeof iconLeft !== 'undefined') ? <span className="pointer-events-none flex items-center w-3 h-3"><Icon className={classNames(
-            Styles['btn-icon'],
-            Styles.icon
-        )} icon={iconLeft} color={iconColor} /></span>:<></>
-    };
-
-    return <ButtonType 
-        href={href ? href :''}
-        id={id}
-        onClick={onClick}
-        disabled={disabled}
-        type={type}
-        className={classNames(
-            wFull,
-            Styles.btn,
-            Styles[btnColor],
-            Styles[btnSize],
-            className,
-        )}
-    >
-        <IconLeft />{label && <span>{label}</span>}<IconRight />
-    </ButtonType>
+export const ButtonLink = ({
+    id,
+    label,
+    full,
+    size = 'base',
+    color = 'primary',
+    className,
+    href,
+    tabIndex,
+    iconLeft,
+    iconRight,
+    iconColor,
+    onClick
+}:{
+    id?:string;
+    label:string | boolean;
+    full?:boolean;
+    size?:keyof ButtonSizes;
+    color?:keyof AMPThemeColors;
+    className?:string;
+    href:string;
+    tabIndex?:any;
+    iconLeft?:keyof AMPThemeIconProps;
+    iconRight?:keyof AMPThemeIconProps;
+    iconColor?:keyof AMPThemeColors;
+    onClick?:any;
+}) => {
+    const btnColor = `btn-${color}`; 
+    const btnSize = `btn-${size}`;
+    const wFull = typeof full !== 'undefined' && 'w-full';
+ 
+    return ( 
+        <Link 
+            id={id}
+            href={href}
+            onClick={onClick}
+            className={classNames(
+                wFull,
+                Styles.btn,
+                Styles[btnColor],
+                Styles[btnSize],
+                className,
+            )}
+        >
+            <IconLeft iconLeft={iconLeft} iconColor={iconColor} />{label && <span>{label}</span>}<IconRight iconRight={iconRight} iconColor={iconColor} />
+        </Link>
+    )
 }
